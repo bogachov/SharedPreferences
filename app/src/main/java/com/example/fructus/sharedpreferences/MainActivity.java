562f4c3edb962344fd7711d81cdd2a4da869ca8f
bogachov <jackwhite33@icloud.com>
2016-01-19 11:01:20 +0200

    Второй вариант

--- app/src/main/java/com/example/fructus/sharedpreferences/MainActivity.java
+++ app/src/main/java/com/example/fructus/sharedpreferences/MainActivity.java
@@ -34,6 +34,11 @@ public class MainActivity extends Activity implements OnClickListener {
 
         btnLoad = (Button) findViewById(R.id.btnLoad);
         btnLoad.setOnClickListener(this);
+
+        sPref = getSharedPreferences("Text",MODE_PRIVATE);
+        String savedText = sPref.getString(SAVED_TEXT, "");
+        etText.setText(savedText);
+        Toast.makeText(this, "Text loaded", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -53,19 +58,31 @@ public class MainActivity extends Activity implements OnClickListener {
     //Метод сохранения текста
     void saveText() {
 
-        //Получаем обьект SharedPreferences который позволяет работать с данными
+        /*//Получаем обьект SharedPreferences который позволяет работать с данными
+        sPref = getSharedPreferences("Text",MODE_PRIVATE);             //MODE_PRIVATE данные доступны только в приложении
+        Editor ed = sPref.edit();                                      //обьект для изменения данных
+        ed.putString(SAVED_TEXT, etText.getText().toString());         //метод для сохранения текста
+        ed.commit();
+        Toast.makeText(this, "Text saved", Toast.LENGTH_SHORT).show();*/
+    }
+
+    //Метод загруски
+    void loadText() {
+        /*sPref = getSharedPreferences("Text",MODE_PRIVATE);
+        String savedText = sPref.getString(SAVED_TEXT, "");
+        etText.setText(savedText);
+        Toast.makeText(this, "Text loaded", Toast.LENGTH_SHORT).show();*/
+    }
+
+    @Override
+    public void onDestroy(){
+
         sPref = getSharedPreferences("Text",MODE_PRIVATE);             //MODE_PRIVATE данные доступны только в приложении
         Editor ed = sPref.edit();                                      //обьект для изменения данных
         ed.putString(SAVED_TEXT, etText.getText().toString());         //метод для сохранения текста
         ed.commit();
         Toast.makeText(this, "Text saved", Toast.LENGTH_SHORT).show();
-    }
 
-    //Метод загруски
-    void loadText() {
-        sPref = getSharedPreferences("Text",MODE_PRIVATE);
-        String savedText = sPref.getString(SAVED_TEXT, "");
-        etText.setText(savedText);
-        Toast.makeText(this, "Text loaded", Toast.LENGTH_SHORT).show();
+        super.onDestroy();
     }
 }
